a0a75fc6b748acb82c730da04c2577cf7da96695
Eric Costello <ecostello71@gmail.com>
2021-04-09 22:40:12 -0400

    fixed one round logic

--- main.js
+++ main.js
@@ -1,78 +1,44 @@
 function computerPlay(){
     choiceNum = Math.floor(Math.random() * (4 - 1 ) ) + 1;
-    let result = "";
-    if (choiceNum == 1){
-        result = "rock";
-    }else if (choiceNum == 2){
-        result = "paper";
-    }else {
-        result = "scissors";
-    }
-    console.log(result);
-    return result;
+    console.log(choiceNum);
+    return choiceNum;
 }
 function playRound(playerSelection, compChoice){
     playerSelection = playerSelection.toLowerCase();
-    let result = "";
-    switch(playerSelection) {
+    switch(playerSelection){
         case "rock":
-            if (compChoice == "rock"){
-                result = "It's a Tie!";
-            }else if (compChoice == "paper"){
-                result = "You lose";
+            if (compChoice == 1){
+                return "Tie";
+            }else if(compChoice == 2){
+                return "You Lose";
             }else{
-                result = "You Win!!!";
+                return "You Win";
             }
             break;
         case "paper":
-            if (compChoice == "paper"){
-                result = "It's a Tie!";
-            }else if (compChoice == "scissors"){
-                result = "You lose";
-            }else{
-                result = "You Win!!!";
-            }
-            break;
-        case "scissors":
-            if (compChoice == "scissors"){
-                result = "It's a Tie!";
-            }else if (compChoice == "rock"){
-                result = "You lose";
-            }else{
-                result = "You Win!!!";
-            }
-            break;
-    }
-    console.log(result);
-}
-function game(){
-    let playerScore = 0;
-    let compScore = 0;
-    let result = "";
-        playRound("ROck", compChoice);
-        switch(result){
-            case "You Win!!!":
-                playerScore += 1;
-                break;
-            case "You lose":
-                compScore += 1;
-                break;
-            default:
-                compScore = compScore;
-            }
-            if (playerScore > compScore){
-                return "You Win!";
+            if (compChoice == 1){
+                return "You Win";
+            }else if(compChoice == 2){
+                return "Tie";
             }else{
                 return "You Lose";
             }
-            console.log(playerScore);
+            break;
+        case "scissors":
+            if (compChoice == 1){
+                return "You Lose";
+            }else if(compChoice == 2){
+                return "You Win";
+            }else{
+                return "Tie";
+            }
+            break;
     }
+        
+}
+
 
 
 let compChoice = computerPlay();
 
-
-
-
-
-console.log(game());
+console.log(playRound("rock", compChoice));
